02601709f263bb2a7c4c48df23f5b8aaf6b33b47
Eric Meeks <eric.meeks@ucsf.edu>
2014-09-03 09:30:19 -0700

    Updated Jena and removed JSON-LD dependency

--- src/main/java/edu/ucsf/ctsi/r2r/jena/JsonLDService.java
+++ src/main/java/edu/ucsf/ctsi/r2r/jena/JsonLDService.java
@@ -1,23 +1,19 @@
 package edu.ucsf.ctsi.r2r.jena;
 
+import java.io.ByteArrayOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.github.jsonldjava.core.JsonLdError;
-import com.github.jsonldjava.core.JsonLdOptions;
-import com.github.jsonldjava.core.JsonLdProcessor;
-import com.github.jsonldjava.jena.JenaRDFParser;
-import com.github.jsonldjava.utils.JSONUtils;
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class JsonLDService {
 
 	private static final Logger LOG = Logger.getLogger(JsonLDService.class.getName());
-
-	private static final String RDFXML = "application/rdf+xml";
 
 	// only use this if the client JSON can know this and take advantage of it easily
 	private String base;
@@ -28,26 +24,22 @@ public class JsonLDService {
 	
 	public JsonLDService(String base) {
 		this.base = base;
-    	JsonLdProcessor.registerRDFParser(RDFXML, new JenaRDFParser());
 	}
 	
 	public String getBase() {
 		return base;
 	}
 	
-	public JSONObject getJSONObject(Model model) throws JSONException, JsonLdError {
+	public JSONObject getJSONObject(Model model) throws JSONException {
         String str = getJSONString(model);
         LOG.log(Level.FINE, str);
         return new JSONObject(str);
 	}
 
-	public String getJSONString(Model model) throws JSONException, JsonLdError {
-        final JsonLdOptions opts = base != null ? new JsonLdOptions(base) : new JsonLdOptions();
-        opts.format = RDFXML;
-        // maybe have outputForm be configurable?
-        opts.outputForm = "compacted";
-    	// do we need to simplify?
-    	Object obj = JsonLdProcessor.fromRDF(model, opts); 
-        return JSONUtils.toString(obj);
+	public String getJSONString(Model model) throws JSONException {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		RDFDataMgr.write(stream, model, RDFFormat.JSONLD);
+		return stream.toString();
 	}
+
 }
